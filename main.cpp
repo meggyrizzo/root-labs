b550b9a49ba1d27538bc698da852f45ff4264705
meggyrizzo <meggy.rizzo@gmail.com>
2025-11-02 10:45:20 +0100

    gm

--- main.cpp
+++ main.cpp
@@ -3,23 +3,26 @@
 #include "mcgenerator.hpp"
 
 int main() {
+  // prova 1
   MCgenerator generate1(10000, 200);
   generate1.GetFunction();
   generate1.CreateHistogram("h", "histo MonteCarlo");
   generate1.Fillh();
   generate1.DrawFunction("function1.png");
-  
+
   TGraphErrors* graph = generate1.GraphMediaConErrore(100);  // 100 repliche
   TCanvas* c = new TCanvas("c_media", "Media con errore", 800, 600);
   graph->Draw("AP");
   c->SaveAs("media_100histo.png");
 
+  // prova 2
   MCgenerator generate2(5000, 300);
   generate2.GetFunction();
   generate2.CreateHistogram("h", "histo MonteCarlo");
   generate2.Fillh();
   generate2.DrawFunction("function2.png");
 
+  // prova 3
   MCgenerator generate3(100000, 100);
   generate3.GetFunction();
   generate3.CreateHistogram("h", "histo MonteCarlo");
