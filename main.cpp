694e5decaf7eaada29a3ada3c7371a4751e5f157
meggyrizzo <meggy.rizzo@gmail.com>
2025-11-04 10:41:52 +0100

    ciaooo

--- main.cpp
+++ main.cpp
@@ -8,19 +8,19 @@ int main() {
   generate1.GetFunction();
   generate1.CreateHistogram("h", "histo MonteCarlo");
   generate1.Fillh();
-  generate1.DrawFunction("function1.png");
+  generate1.DrawFunction("graphs/function1.png");
 
   TGraphErrors* graph = generate1.GraphMeanWithError(100);  // 100 repliche
   TCanvas* c = new TCanvas("c_media", "Media con errore", 800, 600);
   graph->Draw("AP");
-  c->SaveAs("incertezza_rigenerazion_histo.png");
+  c->SaveAs("graphs/uncertainty_rigeneration_histo.png");
 
   // prova 2
   MCgenerator generate2(5000, 300);
   generate2.GetFunction();
   generate2.CreateHistogram("h", "histo MonteCarlo");
   generate2.Fillh();
-  generate2.DrawFunction("function2.png");
+  generate2.DrawFunction("graphs/function2.png");
 
   // prova 3, il migliore, piu vicino alla funzione, è l'histo con + entrate e
   // meno bin
@@ -28,13 +28,13 @@ int main() {
   generate3.GetFunction();
   generate3.CreateHistogram("h", "histo MonteCarlo");
   generate3.Fillh();
-  generate3.DrawFunction("function3.png");
+  generate3.DrawFunction("graphs/function3.png");
 
   MCgenerator smeared(0, 100);  // N=0 perché non serve generare eventi
   TGraphErrors* smear_graph = smeared.GraphBinSmeering(100);  // 100 repliche
   TCanvas* c_smear = new TCanvas("c_smear", "Bin-smeering", 800, 600);
   smear_graph->Draw("AP");
-  c_smear->SaveAs("bin_smeering.png");
+  c_smear->SaveAs("graphs/bin_smeering.png");
 
   return 0;
 }
